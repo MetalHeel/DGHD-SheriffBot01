5cec2e712faa718de92173ab20e45365a6ec8c30
MetalHeel <MetalHeel@aol.com>
2020-03-18 14:03:52 -0500

    Added ability for Sheriff to respond to howdy.

--- dghdSheriffBot.js
+++ dghdSheriffBot.js
@@ -1,13 +1,34 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const botToken = "Njg5ODczNjQyMzY4MDA4Mjg3.XnJPbQ.-0BZElej7k_QOTyS3IDix9zMYyk";
+// The bot token must be passed in, we can't have it public anywhere.
+const args = process.argv.slice(2)
+const botToken = args[0]
+
 const dghdQuarantineGeneralID = "689656654329151613";
+dghdQuarantineGeneral = null;
 
 client.on('ready', () => {
-    console.log("Connected as " + client.user.tag);
+    console.log("Connected as " + client.user.id);
 	
-	client.channels.fetch(dghdQuarantineGeneral).then(channel => channel.send("Hello World!"));
+	client.channels.fetch(dghdQuarantineGeneralID).then(channel => dghdQuarantineGeneral = channel);
+});
+
+client.on('message', message => {
+	if (!dghdQuarantineGeneral) {
+		return;
+	}
+	
+	if (message.author == client.user) {
+        return;
+    }
+	
+	// Use for debugging.
+	// console.log("Content: " + message.content);
+	
+    if ((message.content.startsWith("<@!" + client.user.id + ">") || message.content.startsWith("<@" + client.user.id + ">")) && message.content.toLowerCase().includes("howdy")) {
+        dghdQuarantineGeneral.send("Howdy, partner");
+    }
 });
 
 client.login(botToken);
